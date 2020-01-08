2c89fa6465caf5cc1b577632e866b4716acc3487
Christopher Grassi <cgrassi@iutbayonne.univ-pau.fr>
2020-01-08 16:49:32 +0100

    Création des première routes et modification du template base.html.twig

--- src/Controller/LesRoisDuController.php
+++ src/Controller/LesRoisDuController.php
@@ -8,12 +8,62 @@ use Symfony\Component\Routing\Annotation\Route;
 class LesRoisDuController extends AbstractController
 {
     /**
-     * @Route("/les/rois/du", name="les_rois_du")
+     * @Route("/", name="accueil")
      */
     public function index()
     {
-        return $this->render('les_rois_du/index.html.twig', [
-            'controller_name' => 'LesRoisDuController',
-        ]);
+        return $this->render('les_rois_du/index.html.twig');
     }
+
+    /**
+     * @Route("/game", name="page_principale")
+     */
+    public function affichagePageJeu()
+    {
+        return $this->render('les_rois_du/index.html.twig');
+    }
+    
+    /**
+     * @Route("/createur", name="page_createur")
+     */
+    public function affichagePageCreateur()
+    {
+        return $this->render('les_rois_du/index.html.twig');
+    }
+
+    /**
+     * @Route("/createur/plateaux", name="page_plateaux")
+     */
+    public function affichagePagePlateaux()
+    {
+        return $this->render('les_rois_du/index.html.twig');
+    }
+
+    /**
+     * @Route("/createur/partie", name="page_partie")
+     */
+    public function affichagePagePartie()
+    {
+        return $this->render('les_rois_du/index.html.twig');
+    }
+
+    /**
+     * @Route("/createur/historique", name="page_historique")
+     */
+    public function affichagePageHistorique()
+    {
+        return $this->render('les_rois_du/index.html.twig');
+    }
+
+ 
+    /**
+     * @Route("/createur/partager", name="page_patager")
+     */
+    public function affichagePagePartager()
+    {
+        return $this->render('les_rois_du/index.html.twig');
+    }
+
+
+
 }
