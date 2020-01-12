4c56964738d176048d628c6769d9ce77ba825ab7
Clement Albanel <clement.albanel@gmail.com>
2020-01-12 20:55:48 +0100

    Creation de l'entité Ressource

---
+++ src/Entity/Ressource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RessourceRepository")
+ */
+class Ressource
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $chemin;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getChemin(): ?string
+    {
+        return $this->chemin;
+    }
+
+    public function setChemin(string $chemin): self
+    {
+        $this->chemin = $chemin;
+
+        return $this;
+    }
+}
