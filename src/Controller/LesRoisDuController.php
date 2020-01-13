a60e240bad0f60e8ebd054b85d22316d8ede3222
Clement Albanel <clement.albanel@gmail.com>
2020-01-13 13:04:47 +0100

    Rectification des routes

--- src/Controller/LesRoisDuController.php
+++ src/Controller/LesRoisDuController.php
@@ -16,52 +16,92 @@ class LesRoisDuController extends AbstractController
     }
 
     /**
-     * @Route("/game", name="page_principale")
+     * @Route("/connexion", name="page_connexion")
      */
-    public function affichagePageJeu()
+    public function affichagePageConnexion()
     {
-        return $this->render('les_rois_du/index.html.twig');
+        return $this->render('les_rois_du/connexion.html.twig');
     }
     
     /**
-     * @Route("/createur", name="page_createur")
+     * @Route("/inscription", name="page_inscription")
      */
-    public function affichagePageCreateur()
+    public function affichagePageInscription()
     {
-        return $this->render('les_rois_du/index.html.twig');
+        return $this->render('les_rois_du/inscription.html.twig');
     }
 
     /**
-     * @Route("/createur/plateaux", name="page_plateaux")
+     * @Route("/hub", name="hub")
      */
-    public function affichagePagePlateaux()
+    public function affichageHub()
     {
-        return $this->render('les_rois_du/index.html.twig');
+        return $this->render('les_rois_du/hub.html.twig');
     }
 
     /**
-     * @Route("/createur/partie", name="page_partie")
+     * @Route("/parties", name="espace_partie")
      */
-    public function affichagePagePartie()
+    public function affichageEspacePartie()
     {
-        return $this->render('les_rois_du/index.html.twig');
+        return $this->render('les_rois_du/espacepartie.html.twig');
     }
 
     /**
-     * @Route("/createur/historique", name="page_historique")
+     * @Route("/compte", name="espace_compte")
      */
-    public function affichagePageHistorique()
+    public function affichageEspaceCompte()
     {
-        return $this->render('les_rois_du/index.html.twig');
+        return $this->render('les_rois_du/espacecompte.html.twig');
     }
 
  
     /**
-     * @Route("/createur/partager", name="page_patager")
+     * @Route("/plateaux", name="espace_plateau")
      */
-    public function affichagePagePartager()
+    public function affichageEspacePlateau()
     {
-        return $this->render('les_rois_du/index.html.twig');
+        return $this->render('les_rois_du/espaceplateau.html.twig');
+    }
+
+    /**
+     * @Route("/parties/creation", name="creation_partie")
+     */
+    public function affichageCreationPartie()
+    {
+        return $this->render('les_rois_du/creationPartie.html.twig');
+    }
+
+    /**
+     * @Route("/parties/{idPartie}", name="partie_en_cours")
+     */
+    public function affichagePartieEnCours($idPartie)
+    {
+        return $this->render('les_rois_du/partieencours.html.twig');
+    }
+
+    /**
+     * @Route("/parties/{idPartie}/parametres", name="parametres_partie")
+     */
+    public function affichageParametresPartie($idPartie)
+    {
+        return $this->render('les_rois_du/parametrespartie.html.twig');
+    }
+
+    /**
+     * @Route("/plateaux/{idPlateau}", name="plateau")
+     */
+    public function affichagePlateau($idPlateau)
+    {
+        return $this->render('les_rois_du/plateau.html.twig');
+    }
+
+    /**
+     * @Route("/plateaux/{idPlateau}/parametres", name="parametres_plateau")
+     */
+    public function affichageParametresPlateau($idPlateau)
+    {
+        return $this->render('les_rois_du/parametresplateau.html.twig');
     }
 
 
