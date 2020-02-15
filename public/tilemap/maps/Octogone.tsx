5762271fb8b95429584f0b63bc0ce4e4549283a4
Christopher Grassi <c.grassi@hotmail.fr>
2020-02-15 14:43:24 +0100

    Supression des fichiers inutiles

--- public/tilemap/maps/Octogone.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="Octogone" tilewidth="64" tileheight="64" tilecount="3" columns="3">
- <image source="//jaizkibel/info/cgrassi/Downloads/Octogone.png" width="192" height="64"/>
-</tileset>
