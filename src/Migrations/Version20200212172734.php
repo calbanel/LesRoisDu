990904f55a28060c9c6727c36fc26b88ba8f8bc7
Emma Auzi <auzi.emma@gmail.com>
2020-02-16 15:01:33 +0100

    derniers changements

---
+++ src/Migrations/Version20200212172734.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200212172734 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE partie ADD CONSTRAINT FK_59B1F3D2D3AD62E FOREIGN KEY (plateau_de_jeu_id) REFERENCES plateau_en_jeu (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE partie DROP FOREIGN KEY FK_59B1F3D2D3AD62E');
+    }
+}
