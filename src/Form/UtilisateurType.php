7c69ea3190a746217b8ea31b0102e555a2a929e2
Emma Auzi <auzi.emma@gmail.com>
2020-03-04 13:01:11 +0100

    Traduction des champs du formulaire d'inscription

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -23,8 +23,8 @@ class UtilisateurType extends AbstractType
             ->add('pseudo')
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password'),
+                'first_options'  => array('label' => 'Mot de passe'),
+                'second_options' => array('label' => 'Répétez le mot de passe'),
             ))
         ;
     }
