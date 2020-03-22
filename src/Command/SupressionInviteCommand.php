a9fd89906df361e4e3d953ea449006acfc5418f5
Clement Albanel <clement.albanel@gmail.com>
2020-03-22 17:27:46 +0100

    Commande pour la suppression des invités mise en place

---
+++ src/Command/SupressionInviteCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Doctrine\DBAL\Connection;
+
+class SupressionInviteCommand extends Command
+{
+    protected static $defaultName = 'invite:suppression';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Suppression comptes invités.')
+            ->setHelp('Cette commande permet de supprimer tout les utilisateur invités.')
+        ;
+    }
+
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln([
+        '================================',
+        'Suppression des invités en cours',
+        '================================',
+         ]);
+
+        $this->connection->query('UPDATE plateau_en_jeu SET joueur_id=NULL WHERE joueur_id IN (SELECT id FROM utilisateur WHERE est_invite=1);DELETE FROM utilisateur WHERE est_invite=1;');
+
+        $output->writeln([
+        '================================',
+        'Suppression des invités terminé !',
+        '================================',
+         ]);
+
+
+        return 0;
+    }
+}
