00f1676d8987117958eefc3c058847038e421fe1
Clement Albanel <clement.albanel@gmail.com>
2020-04-04 15:47:08 +0200

    Externalisation formulaire Plateau

---
+++ src/Form/PlateauType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Plateau;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlateauType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('niveauDifficulte')
+            ->add('nbCases')
+            ->add('utilisateurs')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Plateau::class,
+        ]);
+    }
+}
