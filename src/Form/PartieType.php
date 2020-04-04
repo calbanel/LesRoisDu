d8724f1d774577a12840868ba9dbcc8c50cbd8d8
Clement Albanel <clement.albanel@gmail.com>
2020-04-04 16:34:39 +0200

    Modification du formulaire partie + ajout des boutons pour creer plateau

--- src/Form/PartieType.php
+++ src/Form/PartieType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Doctrine\ORM\PlateauRepository;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -27,8 +28,16 @@ class PartieType extends AbstractType
             ->add('nom',TextType::class, ['attr' => ['placeholder' => "Nom de la partie (Il apparaîtra dans l'espace partie des joueurs, veuillez faire attention à ce que vous saisissez)."]])
             ->add('description',TextType::class, ['attr' => ['placeholder' => "Description de la partie (Elle apparaîtra dans l'espace partie des joueurs, veuillez faire attention à ce que vous saisissez)."]])
             ->add('nbPlateaux',IntegerType::class,['data' => '1', 'attr'=> ['readonly'=> true ]])
-            ->add('nbPionParPlateau',RangeType::class,['data' => 1, 'attr'=> ['class' => "RangeCSS", 'min'=> 1, 'max'=>4 ]])
-            ->add('nbFacesDe',RangeType::class,['data' => 1, 'attr'=> ['class' => "RangeCSS", 'min'=> 1, 'max'=>4 ]])
+            ->add('nbPionParPlateau', ChoiceType::class, ['choices'  => [
+                                                            '1' => 1,
+                                                            '2' => 2,
+                                                            '3' => 3,
+                                                            '4' => 4]])
+            ->add('nbFacesDe', ChoiceType::class, ['choices'  => [
+                                                            '1' => 1,
+                                                            '2' => 2,
+                                                            '3' => 3,
+                                                            '4' => 4]])
             ->add('plateau', EntityType::class, [   'class' => Plateau::class,
                                                     'choice_label' => 'nom',
                                                     'multiple' => false,
